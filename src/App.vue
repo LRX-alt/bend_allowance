7f25e6f38dca2f03486697d1b020eeec5aa64644
LRX-alt <lorixdf@gmail.com>
2024-11-28 20:28:42 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    
+    <CalcolatoreLamieraMain />
+  </div>
+</template>
+
+<script>
+import CalcolatoreLamieraMain from "./components/Calcolatore.vue";
+
+export default {
+  components: {
+    CalcolatoreLamieraMain,
+  },
+};
+</script>
